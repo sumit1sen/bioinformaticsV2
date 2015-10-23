64b8d0b7d3968d1deded2be0efc37a815c6dcb2c
sumit1sen <sumit.1.sen@gmail.com>
2015-10-23 16:51:47 -0700

    adding rosalind solutions

--- RosalindRabbits.py
+++ RosalindRabbits.py
@@ -15,4 +15,26 @@ def fibonacciDeath(n,m):
     retVal += fibonacciDeath(n-4,m)
     # retVal = retVal - fibonacciDeath(n-m-1,m)
     return retVal
+
+
+def fibonacciDP(n,m):
+    fib = [-1]*n
+    fib[0] = 1
+    fib[1] = 1
+
+    fibTotal = 0
+    for i in range(0,n):
+        if (i>0):
+            if fib[i-1] == -1:
+                fib[i-1] = getFib(i-1);
+            fibTotal += fib[i-1];
+        if (i>1)
+
+    young=[0]*n
+    old = [0]*n
+    young[0] = 1
+    young[1] = 1
+    for i in range(0,n):
+        if (i<2)
 print fibonacciDeath(5,3)
+
