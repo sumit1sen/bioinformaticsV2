5273c780d0687d94ae93c923fada905a59cfdf52
sumit1sen <sumit.1.sen@gmail.com>
2015-10-28 15:25:58 -0700

    working solution for dying rabbits

--- RosalindRabbits.py
+++ RosalindRabbits.py
@@ -1,40 +1,53 @@
 __author__ = 'ssen'
+import sys
 def fibonacci(n, k):
     if (n<=2):
         return 1
     retVal = fibonacci(n-1,k) + k*fibonacci(n-2,k)
     return retVal
 
-def fibonacciDeath(n,m):
-    if n<=0:
-        return 0
-    elif n<=2:
-        return 1
+def fibonacciDeathV2Loop(n, m, born, mature, dying):
 
-    retVal = fibonacciDeath(n-3,m)
-    retVal += fibonacciDeath(n-4,m)
-    # retVal = retVal - fibonacciDeath(n-m-1,m)
-    return retVal
+    if n==0:
+        born[0] = 1
+        x = []
+        for i in range(m-1):
+            x.append(long(0))
+        mature[0]=x
+        dying[0]=long(0)
+        return long(1)
+    matureTotal = long(0)
+    for i in range(m-1):
+        matureTotal += mature[n-1][i]
+    matureTotal += dying[n-1]
+    born.append(matureTotal)
+    x = [long(0)]
+    x[0]=born[n-1]
+    for i in range(1,m-1):
+        x.append(mature[n-1][i-1])
+    mature.append(x)
+    dying.append(mature[n-1][m-2])
 
+    total = long(0)
+    for i in range(m-1):
+        total += mature[n][i]
+    total += dying[n] + born[n]
+    return total
 
-def fibonacciDP(n,m):
-    fib = [-1]*n
-    fib[0] = 1
-    fib[1] = 1
+def fibonacciDeathV2(n,m):
 
-    fibTotal = 0
-    for i in range(0,n):
-        if (i>0):
-            if fib[i-1] == -1:
-                fib[i-1] = getFib(i-1);
-            fibTotal += fib[i-1];
-        if (i>1)
+    n = n-1
+    m=m-1
+    born = [long(0)]
+    mature = [[]]
+    dying = [long(0)]
+    retval = long(0)
+    for i in range(n+1):
+        retval = fibonacciDeathV2Loop(i,m,born, mature, dying)
+        print retval
 
-    young=[0]*n
-    old = [0]*n
-    young[0] = 1
-    young[1] = 1
-    for i in range(0,n):
-        if (i<2)
-print fibonacciDeath(5,3)
+    print
+    return retval
+
+print fibonacciDeathV2(89,17)
 
